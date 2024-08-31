7f7ba90d2a61c2dd857da67304320aeefaad6626
unknown <adrian..peshev2003@gmail.com>
2024-08-31 17:30:26 +0100

    FIxed bugs

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ let teens=["Eleven","Twelve","Thirteen","Fourteen","FIvteen","Sixteen","Seventee
 let tens=["Ten","Twenty","Thirty","Fourty","Fivty","Sixty","Seventy","Eighty","Ninety"];
 
 let decl=["Thousand","Million","Billion"];
-
 output="";
 let tempParts=[];
 let temp="";
@@ -41,12 +40,19 @@ temp="";
 }
 for(let i=Parts.length-1;i>=0;i--){
 if(Parts[i].length==3){
+if(Parts[i][0]!="0"){
+if(Parts[i][1]!="0" || Parts[i][2]!="0"){
+output+=Once[parseInt(Parts[i][0])-1]+" Hundred and ";
+}else
+{
 output+=Once[parseInt(Parts[i][0])-1]+" Hundred ";
+}
+}
 let temNum="";
 temNum+=Parts[i][1];
 temNum+=Parts[i][2];
 if(parseInt(temNum)<20 && parseInt(temNum)>10){
-output+="and "+teens[parseInt(Parts[i][2])-1];
+output+=teens[parseInt(Parts[i][2])-1];
 }else{
 if(Parts[i][1] !="0" && Parts[i][2]!=0){
 output+=tens[parseInt(Parts[i][1])-1]+" ";
@@ -54,7 +60,7 @@ output+=Once[parseInt(Parts[i][2])-1]+" ";
 }
 else if(Parts[i][1] =="0" && Parts[i][2]!=0)
 {
-output+="and "+Once[parseInt(Parts[i][2])-1]+" ";
+output+=Once[parseInt(Parts[i][2])-1]+" ";
 }else if(Parts[i][1] !="0" && Parts[i][2]==0)
 {
 output+="and "+tens[parseInt(Parts[i][1])-1]+" ";
@@ -62,16 +68,40 @@ output+="and "+tens[parseInt(Parts[i][1])-1]+" ";
 }
 }else if(Parts[i].length==2)
 {
+let tm="";
+tm+=Parts[i][0];
+tm+=Parts[i][1];
+if(parseInt(tm)>10 && parseInt(tm)<20){
+output+=teens[parseInt(Parts[i][1])-1]+" ";
+}else{
+if(Parts[i][1]!="0"){
 output+=tens[parseInt(Parts[i][0])-1]+" ";
 output+=Once[parseInt(Parts[i][1])-1]+" ";
-
+}else
+{
+output+=tens[parseInt(Parts[i][0])-1]+" ";
+}
+}
 }else
 {
 output+=Once[parseInt(Parts[i][0])-1]+" ";
 }
 if(i>0)
 {
+if(Parts[i-1]!="000")
+{
+if(Parts[i]!="000"){
 output+=decl[i-1]+" and ";
+}else
+{
+output+=" and ";
+}
+}else
+{
+if(Parts[i]!="000"){
+output+=decl[i-1] +" ";
+}
+}
 }
 }
 document.getElementById("output").innerText=output;
